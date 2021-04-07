e087c308b26d36584d20e97be389223b454736e5
Dmitrii Korobeinikov <dim1212k@gmail.com>
2021-04-08 00:13:47 +0600

    make-<struct-name> and with-<struct-name>

--- source/extra-bits.lisp
+++ source/extra-bits.lisp
@@ -82,7 +82,90 @@ because they contain unknown abbreviations.  File an issue or add exceptions to
 `catch-questionable-names': ~a." ',*questionable-names*))
     (setf *questionable-names* nil)))
 
+;; * make-<struct-name> and with-<struct-name>
+
+;; Produce make-<struct-name> and with-<struct-name> macros for structs and for
+;; types that point to those structs.  Anon structs are skipped (but not types
+;; that point to them).  If there are no slots, with macros are skipped Keywords
+;; are used in case the original definition or the ordering in it changes.
+
+;; I don't know how to cajole the known info about the generated struct out of
+;; cffi, so I resort to keeping track of this manually.
+(defparameter *known-struct-defs* nil) ; No real need to setf to nil on every generation.
+
+(defmacro def-make-struct-macro (name type-name slot-names)
+  `(defmacro ,name (&key ,@slot-names)
+     (with-gensyms (ptr)
+       `(let ((,ptr (cffi:foreign-alloc ',',type-name)))
+          (with-foreign-slots (,',slot-names ,ptr ,',type-name)
+            (setf ,,@(loop for sn in slot-names appending `(',sn ,sn))))
+          ,ptr))))
+
+(defmacro def-with-struct-macro (name type-name slot-names)
+  `(defmacro ,name ((name &key ,@slot-names) &body body)
+     `(cffi:with-foreign-object (,name ',',type-name)
+        (with-foreign-slots (,',slot-names ,name ,',type-name)
+          (setf ,,@(loop for sn in slot-names appending `(',sn ,sn))))
+        ,@body)))
+
+(defun struct-info-from-defcstruct (form)
+  (values
+   ;; struct name
+   (let ((name (first (second form))))
+     (setf (getf *known-struct-defs* name) form)
+     (unless (cl-ppcre:scan "ANON-STRUCT-" (symbol-name name))
+       name))
+   ;; name
+   (first (second form))
+   ;; slot-names
+   (mapcar #'first (cddr form))))
+
+(defun struct-info-from-defctype (form)
+  (let ((typespec (third form)))
+    (when (listp typespec)
+      (values
+       ;; struct name
+       (when (eql :struct (first typespec))
+         (second typespec))
+       ;; name
+       (second form)
+       ;; stot names
+       (mapcar #'first (cddr (getf *known-struct-defs* (second typespec))))))))
+
+(defparameter *maybe* nil)
+
+(defun maybe-generate-maker-and-with-forms (form)
+  (when (member (first form) '(cffi:defctype cffi:defcstruct))
+    (multiple-value-bind (struct-name name slot-names)
+        (case (first form)
+          (defcstruct (struct-info-from-defcstruct form))
+          (defctype (struct-info-from-defctype form)))
+      (when (and struct-name
+                 ;; don't redefine struct of the same name:
+                 (not (and (eql (first form) 'cffi:defctype) (eql name struct-name)))) 
+        (let ((make-macro-name (symbolicate 'make- name))
+              (with-macro-name (symbolicate 'with- name)))
+          (list*
+           `(eval-when (:compile-toplevel :load-toplevel :execute)
+              (fmakunbound ',make-macro-name)
+              (fmakunbound ',with-macro-name))
+           `(def-make-struct-macro ,make-macro-name ,(list :struct struct-name) ,slot-names)
+           `(export ',make-macro-name)
+           (when slot-names 
+             (list
+              `(def-with-struct-macro ,with-macro-name ,(list :struct struct-name) ,slot-names)
+              `(export ',with-macro-name)))))))))
+
+#+nil
+(cffi:foreign-free (hu.dwim.sdl/core:make-rect :x 0 :y 0 :w 10 :h 10))
+#+nil
+(hu.dwim.sdl/core:with-rect (data :x 0 :y 0 :w 10 :h 10) data)
+
 ;; * Callback setup
+(ql:quickload 'ut)
+
+(defun form-callback (form &key &allow-other-keys)
+  (remove nil (maybe-generate-maker-and-with-forms form)))
 
 (defun prologue-callback (&key &allow-other-keys)
   (remove nil (list (prologue-from-core))))
@@ -92,4 +175,4 @@ because they contain unknown abbreviations.  File an issue or add exceptions to
                     (epilogue-questionable-names))))
 
 (defun callback-factory (&key &allow-other-keys)
-  (values nil #'epilogue-callback #'prologue-callback))
+  (values #'form-callback #'epilogue-callback #'prologue-callback))
